83d7a5ef3441a97e8b037be09852064271b0b27c
Pratikviswakarma <pratikvishwakarma1812@gmail.com>
2025-02-08 11:32:40 +0530

    [Pratik] Add. Compare StringBuilder And Stringbuffer Solution

---
+++ Day4/StringBuffer/src/test/java/CompareSbAndSbfTesting.java
@@ -0,0 +1,11 @@
+import CompareSbAndSbf.CompareSbAndSbf;
+
+import org.junit.jupiter.api.Test;
+
+public class CompareSbAndSbfTesting {
+    @Test
+    void comsbsdf(){
+        CompareSbAndSbf csbsbf=new CompareSbAndSbf();
+        csbsbf.compareWithTime("Hello");
+    }
+}
